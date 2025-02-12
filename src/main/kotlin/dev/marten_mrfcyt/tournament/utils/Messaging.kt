748e02882ee92ca5ebc9d7278946c11015c9326f
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2025-02-12 13:56:04 +0100

    [Plugin] Improve messaging a bit

--- src/main/kotlin/dev/marten_mrfcyt/tournament/utils/Messaging.kt
+++ src/main/kotlin/dev/marten_mrfcyt/tournament/utils/Messaging.kt
@@ -3,24 +3,23 @@ package dev.marten_mrfcyt.tournament.utils
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.command.CommandSender
-import org.bukkit.entity.Player
 
 private val mm = MiniMessage.builder().build()
 
-fun String.asMini(player: Player? = null): Component {
+fun String.asMini(): Component {
     return mm.deserialize("<!i>$this")
 }
 
-fun CommandSender.sendMini(message: String, player: Player? = null) {
+fun CommandSender.sendMini(message: String) {
     sendMessage(message.asMini())
 }
 
-fun CommandSender.error(message: String, player: Player? = null) {
+fun CommandSender.error(message: String) {
     val formattedMessage = "<red><bold>Error</bold><gray> | <white> $message"
     sendMessage(formattedMessage.asMini())
 }
 
-fun CommandSender.message(message: String, player: Player? = null) {
+fun CommandSender.message(message: String) {
     val formattedMessage = "<gold><bold>Tournaments</bold><gray> | <white> $message"
     sendMessage(formattedMessage.asMini())
 }
